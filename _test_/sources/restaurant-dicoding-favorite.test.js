8965bd4677e4f0d589c9c248ce95e2bc48f23be7
Dimas Maulana Dwi Saputra <dimas.maulanads@gmail.com>
2020-08-11 18:12:52 +0700

    Add restaurant dicoding favorite test for get all restaurants

---
+++ _test_/sources/restaurant-dicoding-favorite.test.js
@@ -0,0 +1,24 @@
+import { GetFavoriteRestaurantsObjectMock } from './mock/favorites-object-mock';
+import IdbDatabase from '../../src/scripts/apis/databases/idb-database';
+import RestaurantDicodingFavorite from '../../src/scripts/sources/restaurant-dicoding-favorite';
+
+describe('Restaurant Dicoding Favorite Source', () => {
+  describe('Get All Favorite Restaurants', () => {
+    it('should return all favorite restaurants correctly', () => {
+      const idbDatabase = new IdbDatabase();
+      const mockDatabase = jest.spyOn(idbDatabase, 'getAll')
+        .mockImplementation(() => ({
+          restaurants: GetFavoriteRestaurantsObjectMock,
+        }));
+
+      const restaurantDicodingFavorite = new RestaurantDicodingFavorite(idbDatabase);
+      const restaurants = restaurantDicodingFavorite.getRestaurants();
+
+      expect(mockDatabase)
+        .toBeCalled();
+
+      expect(restaurants)
+        .toStrictEqual(GetFavoriteRestaurantsObjectMock);
+    });
+  });
+});
