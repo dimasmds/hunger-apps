f89d041d3b364f6ecfa3811081653010705914bd
Dimas Maulana Dwi Saputra <dimas.maulanads@gmail.com>
2020-08-14 16:13:14 +0700

    Extract error message for database

--- src/scripts/apis/databases/idb-database.js
+++ src/scripts/apis/databases/idb-database.js
@@ -1,4 +1,5 @@
 import { openDB } from 'idb';
+import ExceptionMessages from '../../globals/ExceptionMessages';
 
 class IdbDatabase {
   constructor({ databaseName = 'default-db', databaseVersion = 1 } = {}) {
@@ -19,20 +20,20 @@ class IdbDatabase {
       const database = await this._open();
       await database.put(objectStoreName, value);
     } catch (error) {
-      throw new Error(`Value not meet specification to put to database see: ${error}`);
+      throw new Error(ExceptionMessages.Apis.Database.INVALID_VALUE_SPECIFICATION_WITH_ERR(error));
     }
   }
 
   async get({ objectStoreName = 'default', key } = {}) {
     if (!key) {
-      throw new Error('Please specify key to get a single data');
+      throw new Error(ExceptionMessages.Apis.Database.EMPTY_KEY_PARAMETER_GET_ERR);
     }
 
     try {
       const database = await this._open();
       return await database.get(objectStoreName, key);
     } catch (error) {
-      throw new Error(`Failed to get single data, see: ${error}`);
+      throw new Error(ExceptionMessages.Apis.Database.GENERIC_GET_WITH_ERR(error));
     }
   }
 
@@ -41,20 +42,20 @@ class IdbDatabase {
       const database = await this._open();
       return await database.getAll(objectStoreName);
     } catch (error) {
-      throw new Error(`Failed to get all data, see: ${error}`);
+      throw new Error(ExceptionMessages.Apis.Database.GENERIC_GET_ALL_WITH_ERR(error));
     }
   }
 
   async delete({ objectStoreName = 'default', key } = {}) {
     if (!key) {
-      throw new Error('Please specify key to delete a single data');
+      throw new Error(ExceptionMessages.Apis.Database.EMPTY_KEY_PARAMETER_DELETE_ERR);
     }
 
     try {
       const database = await this._open();
       await database.delete(objectStoreName, key);
     } catch (error) {
-      throw new Error(`Failed to delete single data, see: ${error}`);
+      throw new Error(ExceptionMessages.Apis.Database.GENERIC_DELETE_WITH_ERR(error));
     }
   }
 
--- src/scripts/globals/ExceptionMessages.js
+++ src/scripts/globals/ExceptionMessages.js
@@ -7,6 +7,10 @@ const ExceptionMessages = {
     Database: {
       EMPTY_KEY_PARAMETER_GET_ERR: 'Please specify key to get a single data',
       EMPTY_KEY_PARAMETER_DELETE_ERR: 'Please specify key to delete a single data',
+      INVALID_VALUE_SPECIFICATION_WITH_ERR: (error) => `Value not meet specification to put to database see: ${error}`,
+      GENERIC_GET_WITH_ERR: (error) => `Failed to get single data, see: ${error}`,
+      GENERIC_GET_ALL_WITH_ERR: (error) => `Failed to get all data, see: ${error}`,
+      GENERIC_DELETE_WITH_ERR: (error) => `Failed to delete single data, see: ${error}`,
 
     },
   },
