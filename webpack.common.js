c34773a0fca77a95664c9eccaaabe4b8fa38a531
Dimas Maulana Dwi Saputra <dimas.maulanads@gmail.com>
2020-08-20 14:31:55 +0700

    Adding Custom Elements polyfill

--- webpack.common.js
+++ webpack.common.js
@@ -1,12 +1,42 @@
-const path = require('path');
+const { resolve } = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+const webcomponentsjs = './node_modules/@webcomponents/webcomponentsjs';
+
+const polyfills = [
+  {
+    from: resolve(`${webcomponentsjs}/webcomponents-bundle.js`),
+    to: 'vendor',
+    flatten: true,
+  },
+  {
+    from: resolve(`${webcomponentsjs}/webcomponents-bundle.js.map`),
+    to: 'vendor',
+    flatten: true,
+  },
+  {
+    from: resolve(`${webcomponentsjs}/webcomponents-loader.js`),
+    to: 'vendor',
+    flatten: true,
+  },
+  {
+    from: resolve(`${webcomponentsjs}/bundles/`),
+    to: 'vendor/bundles',
+    flatten: true,
+  },
+  {
+    from: resolve(`${webcomponentsjs}/custom-elements-es5-adapter.js`),
+    to: 'vendor',
+    flatten: true,
+  },
+];
+
 module.exports = {
-  entry: path.resolve(__dirname, 'src/scripts/index.js'),
+  entry: resolve(__dirname, 'src/scripts/index.js'),
   output: {
     filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: resolve(__dirname, 'dist'),
   },
   module: {
     rules: [
@@ -25,14 +55,15 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'src/templates/index.html'),
+      template: resolve(__dirname, 'src/templates/index.html'),
       filename: 'index.html',
     }),
     new CopyWebpackPlugin({
       patterns: [
+        ...polyfills,
         {
-          from: path.resolve(__dirname, 'src/public/'),
-          to: path.resolve(__dirname, 'dist/'),
+          from: resolve(__dirname, 'src/public/'),
+          to: resolve(__dirname, 'dist/'),
         },
       ],
     }),
