31276a8d85ba538d4be72fd6015408f79ed29296
Dimas Maulana Dwi Saputra <dimas.maulanads@gmail.com>
2020-08-27 21:09:43 +0700

    Preparing RestaurantDetail components

---
+++ src/scripts/presentations/components/RestaurantDetail/index.js
@@ -0,0 +1,45 @@
+import { html } from 'lit-html';
+import CommonElement from '../_base_/CommonElement';
+import style from './style.scss';
+
+class RestaurantDetail extends CommonElement {
+  static get properties() {
+    return {
+      _restaurant: { type: Object },
+    };
+  }
+
+  static get styles() {
+    return [...super.styles, style];
+  }
+
+  setRestaurant(restaurant) {
+    this._restaurant = restaurant;
+  }
+
+  constructor() {
+    super();
+    this._restaurant = null;
+  }
+
+  render() {
+    if (this._restaurant) {
+      return this._renderRestaurant();
+    }
+
+    return this._renderSkeleton();
+  }
+
+  _renderRestaurant() {
+    const { name } = this._restaurant;
+    return html`
+      <p>${name}</p>
+    `;
+  }
+
+  _renderSkeleton() {
+    return html`<p>Loading ...</p>`;
+  }
+}
+
+customElements.define('restaurant-detail', RestaurantDetail);
--- src/scripts/presentations/components/index.js
+++ src/scripts/presentations/components/index.js
@@ -2,3 +2,4 @@ import './AppBar';
 import './HeroElement';
 import './FootBar';
 import './RestaurantsContainer';
+import './RestaurantDetail';
