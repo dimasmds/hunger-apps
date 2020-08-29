797fa9389e2f17975e76b141614f0ff41cb25f37
Dimas Saputra <dimas@dicoding.com>
2020-08-29 12:59:40 +0700

    Styling minimal detail

--- src/scripts/presentations/components/RestaurantDetail/index.js
+++ src/scripts/presentations/components/RestaurantDetail/index.js
@@ -1,6 +1,9 @@
 import { html } from 'lit-html';
 import CommonElement from '../_base_/CommonElement';
 import style from './style.scss';
+import responsive from './responsive.scss';
+import ImageUrlGenerator from '../../../utils/image-url-generator';
+import AppConfig from '../../../globals/app-config';
 
 class RestaurantDetail extends CommonElement {
   static get properties() {
@@ -10,7 +13,7 @@ class RestaurantDetail extends CommonElement {
   }
 
   static get styles() {
-    return [...super.styles, style];
+    return [...super.styles, style, responsive];
   }
 
   setRestaurant(restaurant) {
@@ -43,10 +46,38 @@ class RestaurantDetail extends CommonElement {
   }
 
   _renderRestaurant() {
-    const { name } = this._restaurant;
+    const {
+      id, name, pictureId, address, categories, city, consumerReviews, description, menus, rating,
+    } = this._restaurant;
     return html`
       <div id="restaurantDetail" class="restaurant-detail">
-         <p>${name}</p>
+        <div class="restaurant-detail__header">
+            <img class="restaurant-detail__header__image" 
+                 alt="${name}" 
+                 src="${ImageUrlGenerator.generate(pictureId, AppConfig.imageQuality.LARGE)}"
+                 srcset="${ImageUrlGenerator.generate(pictureId, AppConfig.imageQuality.SMALL)} 480w, 
+                         ${ImageUrlGenerator.generate(pictureId)} 800w,
+                         ${ImageUrlGenerator.generate(pictureId, AppConfig.imageQuality.LARGE)} 1200w"
+                 sizes="(max-width: 799px) 480w,
+                        (max-width: 1199px) 800w,
+                        1200w">
+        </div>
+        <div class="restaurant-detail__content">
+            <h2 class="restaurant-detail__content__title">${name}</h2>
+            <div class="restaurant-detail__content__detail">
+                <span class="restaurant-detail__content__detail__item">
+                    <img class="icon-small" alt="location" src="./images/icons/location.svg">${`${address}, ${city}.`}
+                </span>
+                <span class="restaurant-detail__content__detail__item">
+                    <img class="icon-small" alt="rating" src="./images/icons/rating.svg">${rating}   
+                </span>
+                <span class="restaurant-detail__content__detail__item">
+                    <img class="icon-small" alt="categories" src="./images/icons/categories.svg">
+                    ${categories.map((category) => `${category.name}`).join(', ')}  
+                </span>
+            </div>
+            <p>${description}</p>
+        </div>
       </div>
     `;
   }
