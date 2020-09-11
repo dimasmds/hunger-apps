a73cd43448fd0f0de9b9b91f74bac11d9463ee6d
Dimas Maulana Dwi Saputra <dimas.maulanads@gmail.com>
2020-09-11 14:14:47 +0700

    Remove double backslash in endpoint

--- src/scripts/globals/api-endpoint.js
+++ src/scripts/globals/api-endpoint.js
@@ -1,9 +1,9 @@
 import AppConfig from './app-config';
 
 const Endpoint = {
-  getAllRestaurants: () => `${AppConfig.BASE_API_URL}/list`,
-  getRestaurant: (id) => `${AppConfig.BASE_API_URL}/detail/${id}`,
-  postCustomerReview: () => `${AppConfig.BASE_API_URL}/review`,
+  getAllRestaurants: () => `${AppConfig.BASE_API_URL}list`,
+  getRestaurant: (id) => `${AppConfig.BASE_API_URL}detail/${id}`,
+  postCustomerReview: () => `${AppConfig.BASE_API_URL}review`,
 };
 
 export default Endpoint;
