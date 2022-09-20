7951d70553344d93c4f6177777477536774d53a8
Dimas Maulana Dwi Saputra <dimas.maulanads@gmail.com>
2022-09-20 10:58:54 +0700

    migrate to lit

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   testEnvironment: 'jest-environment-jsdom-sixteen',
   transformIgnorePatterns: [
-    '/node_modules/(?!lit-element|lit-html)',
+    '/node_modules/(?!lit|lit-html)',
   ],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js',
--- src/scripts/presentations/components/_base_/CommonElement/index.js
+++ src/scripts/presentations/components/_base_/CommonElement/index.js
@@ -1,4 +1,4 @@
-import { LitElement } from 'lit-element';
+import { LitElement } from 'lit';
 import style from './style.scss';
 
 class CommonElement extends LitElement {
